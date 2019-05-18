9849f79d3af25a30b44d64f532aa88865832b97a
Christian Spanring <cspanring@gmail.com>
2019-05-17 21:08:38 -0400

    adds basic Vue app scaffolding

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Backcountry Buddy</h1>
+    <Tours/>
+  </div>
+</template>
+
+<script>
+import Tours from './components/Tours.vue'
+
+export default {
+  name: 'app',
+  components: { 
+    Tours
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Tours.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Tours</h2>
+    <ul>
+      <li v-for="(tour, index) in tours"  v-bind:key="index">
+        {{tour.description}}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import { db } from '@/config/db'
+export default {
+  data: function () {
+    return {
+       tours: [],
+    }
+  },
+
+  firestore: {
+    tours: db.collection('tours'),
+  },
+}
+</script>
+
+<style scoped>
+</style>
