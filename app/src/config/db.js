facde6b9e505af1f74aacfdf43eb5acdb517bdee
Christian Spanring <cspanring@gmail.com>
2019-08-27 19:09:45 -0400

    firebase optimization

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,11 +1,10 @@
-import firebase from 'firebase'
-// Get a Firestore instance
+import firebase from 'firebase/app';
+import 'firebase/firebase-firestore';
+
+// firestore instance
 export const db = firebase
   .initializeApp({ projectId: 'backcountry-buddy' })
   .firestore();
 
 // Export types that exists in Firestore
 // export { TimeStamp, GeoPoint } = firebase.firestore;
-
-// if using Firebase JS SDK < 5.8.0
-// db.settings({ timestampsInSnapshots: true })
