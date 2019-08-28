ee576eebc875da4f9954e90ace4b4109cd6c3b82
Chris George <chris.a.george@gmail.com>
2019-08-27 17:49:26 -0700

    Login stuff

--- app/src/api/request.js
+++ app/src/api/request.js
@@ -2,7 +2,14 @@ import fetch from 'node-fetch';
 
 export default class BackCountryRequest {
   static baseUrl = 'https://givemeenvar';
-  static routes = {
 
-  };
+  static login(email, password) {
+    // do some login stuff from db.
+  }
+
+  static signup(email, password) {
+    // salt and encrypt password
+    // store email and password
+    // create profile
+  }
 }
