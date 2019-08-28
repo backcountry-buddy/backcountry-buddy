5e552f17db8644aa7deca027ef0dc4fe1adfcb2f
Christian Spanring <cspanring@gmail.com>
2019-08-27 20:13:17 -0400

    CSS optimizations

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,6 +1,16 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./src/**/*.html", "./src/**/*.vue"],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+      : ""
+  ]
 };
--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="p-4 lg:mx-auto lg:w-1/2">
+  <div id="app" class="font-sans p-4 lg:mx-auto lg:w-1/2">
     <h1 class="text-xl font-semibold text-center">Backcountry Buddy</h1>
     <TourList />
   </div>
@@ -15,9 +15,3 @@ export default {
   }
 };
 </script>
-
-<style>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-</style>
